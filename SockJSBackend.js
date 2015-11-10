5ee7d02a5b6ff93a2be242febb75ea1639513919
Bret Little <bret.little@gmail.com>
2015-11-09 22:20:12 -0700

    Add SockJSBackend for the client

---
+++ SockJSBackend.js
@@ -0,0 +1,61 @@
+import { Observable } from 'rx';
+import SockJS from 'sockjs-client';
+
+let sock;
+
+function getTestUrl(url) {
+	let parser = document.createElement('a');
+	parser.href = url;
+	return parser.protocol + '//' + parser.host + parser.pathname + '/info' + parser.search;
+}
+
+function tryConnect(url, observer) {
+	sock = new SockJS(url);
+
+	sock.onopen = function() {
+		observer.onNext();
+	}
+
+	sock.onclose = function() {
+		fetch(getTestUrl(url), {
+			method: 'get',
+			credentials: 'include'
+		}).then((response) => {
+			if (response.status === 401) {
+				observer.onError('Unauthorized');
+			} else {
+				observer.onError('Lost connection');
+			}
+		}).catch(() => {
+			observer.onError('Lost connection');
+		});
+	}
+
+	sock.onerror = function(e) {
+		observer.onError(e)
+	}
+}
+
+export default {
+
+	connect(url) {
+		return Observable.create((observer) => {
+			tryConnect(url, observer);
+		})
+	},
+
+	write(request) {
+		sock.send(request);
+	},
+
+	onMessage(callback) {
+		sock.onmessage = (message) => {
+			callback.call(null, message.data);
+		}
+	},
+
+	close() {
+		sock.close();
+	}
+}
+
